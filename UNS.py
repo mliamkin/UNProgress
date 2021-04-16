378b3cbfd719ccd6858154c7b56c402b5498c6c9
Michael Liamkin <67211068+mliamkin@users.noreply.github.com>
2021-04-16 02:39:23 -0400

    Update UNS.py

--- UNS.py
+++ UNS.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
+import requests
+import json
+
 app = Flask(__name__)
+
+r = requests.get('https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=true')
+dataGoals = json.loads(r.text)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/goals')
+def goals():
+    return render_template('goals.html', goalList=dataGoals)
